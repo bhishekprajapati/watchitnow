672de06ad4d37e4001c886004c9b8ec33f04b849
Abhishek Prajapati <bhishekprajapati@gmail.com>
2023-11-10 22:09:24 +0530

    update navbar

--- src/app/app/NavBar.js
+++ src/app/app/NavBar.js
@@ -1,12 +1,34 @@
+"use client";
+
 import Image from "next/image";
 import Link from "next/link";
+import {
+  IconHome,
+  IconMovie,
+  IconDeviceTv,
+  IconBookmark,
+} from "@tabler/icons-react";
+import { usePathname } from "next/navigation";
 
 export default function NavBar() {
+  const pathName = usePathname();
+
   return (
-    <nav className="px-4 py-[1.12rem] bg-semi-dark-blue md:rounded-2xl lg:h-full lg:px-8 lg:py-9">
+    <nav className="px-4 py-[1.12rem] bg-semi-dark-blue md:rounded-2xl lg:h-full lg:px-8 lg:py-9 flex lg:flex-col justify-between">
       <Link className="w-6 h-[1.125rem] md:w-8 md:h-[1.6rem]" href="/">
         <Image width={24} height={24} src="/logo.svg" alt="site-logo" />
       </Link>
+
+      <ul>
+        <li>
+          <Link href="/app">
+            <IconHome
+              className={`nav-link ${pathName === "/app" ? "active" : ""}`}
+              fill="true"
+            />
+          </Link>
+        </li>
+      </ul>
     </nav>
   );
 }
