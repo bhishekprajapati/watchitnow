7c49301b9f7ba7d1498aad9ca3b4781c9d6491e8
Abhishek Prajapati <bhishekprajapati@gmail.com>
2023-11-15 07:20:59 +0530

    update title

--- src/app/app/bookmarks/page.js
+++ src/app/app/bookmarks/page.js
@@ -1,3 +1,7 @@
+export const metadata = {
+  title: "Bookmarks",
+};
+
 export default function Page() {
   return <></>;
 }
--- src/app/app/movies/page.js
+++ src/app/app/movies/page.js
@@ -1,3 +1,7 @@
+export const metadata = {
+  title: "Movies",
+};
+
 export default function Page() {
   return <></>;
 }
