83a0afadce21caf58be2dfbd6b83b3677ddbb77b
Abhishek Prajapati <bhishekprajapati@gmail.com>
2023-11-19 12:34:54 +0530

    fix search form to behave same, consistently across all the pages

--- src/app/app/Search.js
+++ src/app/app/Search.js
@@ -3,13 +3,13 @@
 import { usePathname, useRouter } from "next/navigation";
 import { IconSearch } from "@tabler/icons-react";
 
-function SearchForm({ placeholder, mediaType }) {
+function SearchForm({ placeholder }) {
   const router = useRouter();
 
   function handleSubmit(e) {
     e.preventDefault();
     const formData = new FormData(e.target);
-    router.push(`search?type=${mediaType}&slug=${formData.get("query")}`);
+    router.push(`/app/search?type=multi&slug=${formData.get("query")}`);
   }
 
   return (
@@ -26,46 +26,19 @@ function SearchForm({ placeholder, mediaType }) {
         placeholder={placeholder}
       />
 
-      <span className="-translate-x-[100%] transition-transform border border-greyish-blue block w-full"></span>
+      <span className="-translate-x-[100%] transition-transform border border-greyish-blue/25 block w-full"></span>
     </form>
   );
 }
 
 export default function Search() {
-  const pathname = usePathname();
-  let placeholder;
-  let mediaType;
-
-  switch (pathname) {
-    case "/app/movies":
-      placeholder = "Search for movies";
-      mediaType = "movie";
-      break;
-
-    case "/app/tv-series":
-      placeholder = "Search for TV series";
-      mediaType = "tv";
-      break;
-
-    case "/app/bookmarks":
-      placeholder = "Search from bookmarked";
-      break;
-
-    default:
-      placeholder = "Search for movies and TV series";
-      mediaType = "multi";
-  }
-
+  const path = usePathname();
   return (
     <div className="flex gap-x-6">
       <label htmlFor="search" className="cursor-pointer mt-[0.5rem]">
         <IconSearch className="text-white" stroke={2.5} />
       </label>
-      <SearchForm
-        key={pathname}
-        mediaType={mediaType}
-        placeholder={placeholder}
-      />
+      <SearchForm key={path} placeholder="Search for movies and TV series" />
     </div>
   );
 }
