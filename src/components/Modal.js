50e51e4096a8a374df62f6debf02360cf2fa6d85
Abhishek Prajapati <bhishekprajapati@gmail.com>
2023-11-20 12:02:10 +0530

    update

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -3,7 +3,7 @@ import classNames from "classnames";
 export default function Modal({ children, show }) {
   const modalClasses = classNames(
     "fixed z-50 left-[25%] right-[25%] top-[15%] translate-y-[30%] overflow-hidden",
-    "h-[50vh] lg:h-[60vh] rounded-md",
+    "h-[50vh] lg:h-[60vh] rounded-xl",
     "bg-gradient-to-tl from-semi-dark-blue/75 to-dark-blue backdrop-blur-3xl",
     "shadow-2xl shadow-black border-2 border-dark-blue",
     "opacity-0 pointer-events-none",
