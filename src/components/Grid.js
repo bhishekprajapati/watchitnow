d0434f272d65167f9afa1a669b1447ae80eae912
Abhishek Prajapati <bhishekprajapati@gmail.com>
2023-11-29 09:04:36 +0530

    update

--- src/components/Grid.js
+++ src/components/Grid.js
@@ -3,13 +3,23 @@ import classNames from "classnames";
 import { forwardRef } from "react";
 
 const Grid = forwardRef(
-  ({ as: Element = "ul", className, children, ...props }, ref) => {
-    const classes = classNames(
+  ({ as: Element = "ul", className, children, strict, ...props }, ref) => {
+    const scrollableLayoutClasses = classNames(
       "overflow-x-auto",
       "scrollbar-none [@media(hover:hover)]:scrollbar [@media(hover:hover)]:pb-2",
       "whitespace-nowrap lg:whitespace-normal [&>*]:inline-block",
-      "[&>*]:w-24 sm:[&>*]:w-28 md:[&>*]:w-36 [&>:not(:last-child)]:mr-2",
-      "lg:[&>*]:w-full lg:[&>:not(:last-child)]:mr-0",
+      "[&>*]:w-24 sm:[&>*]:w-28 md:[&>*]:w-36 [&>:not(:last-child)]:mr-3",
+      "lg:[&>*]:w-full lg:[&>:not(:last-child)]:mr-0"
+    );
+
+    const gridLayoutClasses = classNames(
+      "grid grid-cols-3 gap-4",
+      "sm:grid-cols-4",
+      "md:grid-cols-5 md:gap-6"
+    );
+
+    const classes = classNames(
+      strict ? gridLayoutClasses : scrollableLayoutClasses,
       "lg:grid lg:grid-cols-5 lg:gap-6",
       "xl:grid-cols-6",
       "2xl:grid-cols-7"
