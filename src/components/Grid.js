937b5f9f742e448357e20febb0d60a77d61e5d07
Abhishek Prajapati <bhishekprajapati@gmail.com>
2023-11-29 22:58:23 +0530

    refactor

--- src/components/Grid.js
+++ src/components/Grid.js
@@ -2,35 +2,36 @@ import { twMerge } from "tailwind-merge";
 import classNames from "classnames";
 import { forwardRef } from "react";
 
-const Grid = forwardRef(
-  ({ as: Element = "ul", className, children, strict, ...props }, ref) => {
-    const scrollableLayoutClasses = classNames(
-      "overflow-x-auto",
-      "scrollbar-none [@media(hover:hover)]:scrollbar [@media(hover:hover)]:pb-2",
-      "whitespace-nowrap lg:whitespace-normal [&>*]:inline-block",
-      "[&>*]:w-24 sm:[&>*]:w-28 md:[&>*]:w-36 [&>:not(:last-child)]:mr-3",
-      "lg:[&>*]:w-full lg:[&>:not(:last-child)]:mr-0"
-    );
+function Grid(
+  { as: Element = "ul", className, children, strict, ...props },
+  ref
+) {
+  const scrollableLayoutClasses = classNames(
+    "overflow-x-auto",
+    "scrollbar-none [@media(hover:hover)]:scrollbar [@media(hover:hover)]:pb-2",
+    "whitespace-nowrap lg:whitespace-normal [&>*]:inline-block",
+    "[&>*]:w-24 sm:[&>*]:w-28 md:[&>*]:w-36 [&>:not(:last-child)]:mr-3",
+    "lg:[&>*]:w-full lg:[&>:not(:last-child)]:mr-0"
+  );
 
-    const gridLayoutClasses = classNames(
-      "grid grid-cols-3 gap-4",
-      "sm:grid-cols-4",
-      "md:grid-cols-5 md:gap-6"
-    );
+  const gridLayoutClasses = classNames(
+    "grid grid-cols-3 gap-4",
+    "sm:grid-cols-4",
+    "md:grid-cols-5 md:gap-6"
+  );
 
-    const classes = classNames(
-      strict ? gridLayoutClasses : scrollableLayoutClasses,
-      "lg:grid lg:grid-cols-5 lg:gap-6",
-      "xl:grid-cols-6",
-      "2xl:grid-cols-7"
-    );
+  const classes = classNames(
+    strict ? gridLayoutClasses : scrollableLayoutClasses,
+    "lg:grid lg:grid-cols-5 lg:gap-6",
+    "xl:grid-cols-6",
+    "2xl:grid-cols-7"
+  );
 
-    return (
-      <Element className={twMerge(classes, className)} ref={ref} {...props}>
-        {children}
-      </Element>
-    );
-  }
-);
+  return (
+    <Element className={twMerge(classes, className)} ref={ref} {...props}>
+      {children}
+    </Element>
+  );
+}
 
-export default Grid;
+export default forwardRef(Grid);
