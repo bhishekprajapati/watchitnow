e8dd36c84bd0e772c2a88aca4fb501797a2a2e7d
Abhishek Prajapati <bhishekprajapati@gmail.com>
2023-12-02 20:51:16 +0530

    refactor grid component

--- src/components/Grid.js
+++ src/components/Grid.js
@@ -1,37 +1,29 @@
 import { twMerge } from "tailwind-merge";
 import classNames from "classnames";
-import { forwardRef } from "react";
 
-function Grid(
-  { as: Element = "ul", className, children, strict, ...props },
-  ref
-) {
-  const scrollableLayoutClasses = classNames(
-    "overflow-x-auto",
-    "scrollbar-none [@media(hover:hover)]:scrollbar [@media(hover:hover)]:pb-2",
-    "whitespace-nowrap lg:whitespace-normal [&>*]:inline-block",
-    "[&>*]:w-24 sm:[&>*]:w-28 md:[&>*]:w-36 [&>:not(:last-child)]:mr-3",
-    "lg:[&>*]:w-full lg:[&>:not(:last-child)]:mr-0"
-  );
-
-  const gridLayoutClasses = classNames(
+function Grid({ as: GridContainer = "ul", className, children, ...props }) {
+  const classes = classNames(
     "grid grid-cols-3 gap-4",
     "sm:grid-cols-4",
-    "md:grid-cols-5 md:gap-6"
-  );
-
-  const classes = classNames(
-    strict ? gridLayoutClasses : scrollableLayoutClasses,
+    "md:grid-cols-5 md:gap-6",
     "lg:grid lg:grid-cols-5 lg:gap-6",
     "xl:grid-cols-6",
     "2xl:grid-cols-7"
   );
 
   return (
-    <Element className={twMerge(classes, className)} ref={ref} {...props}>
+    <GridContainer className={twMerge(classes, className)} {...props}>
       {children}
-    </Element>
+    </GridContainer>
   );
 }
 
-export default forwardRef(Grid);
+Grid.Item = ({ as: GridItem = "li", children, className, ...props }) => {
+  return (
+    <GridItem className={className} {...props}>
+      {children}
+    </GridItem>
+  );
+};
+
+export default Grid;
