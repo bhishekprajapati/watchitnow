006b28d3a7c81a2743a3b39a48afc632d3cf33f7
Abhishek Prajapati <bhishekprajapati@gmail.com>
2023-12-03 10:28:30 +0530

    update

--- src/components/Ambience.js
+++ src/components/Ambience.js
@@ -2,12 +2,12 @@ export default function Ambience({}) {
   return (
     <>
       <img
-        className="fixed z-0 top-0 left-[-25%] opacity-75 animate-breathe"
+        className="fixed z-0 top-0 left-[-25%] opacity-75 animate-breathe pointer-events-none"
         src="/ambience-left.png"
         loading="lazy"
       />
       <img
-        className="fixed z-0 top-[50%] lg:top-0 right-[-25%] md:right-[-50%] opacity-50 animate-breathe"
+        className="fixed z-0 top-[50%] lg:top-0 right-[-25%] md:right-[-50%] opacity-50 animate-breathe pointer-events-none"
         src="/ambience-right.png"
         loading="lazy"
       />
