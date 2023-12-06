11d0cd7696faf351907e7d8acae95648803f7021
Abhishek Prajapati <bhishekprajapati@gmail.com>
2023-12-06 18:25:48 +0530

    update bg effects

--- src/components/Ambience.js
+++ src/components/Ambience.js
@@ -2,13 +2,13 @@ export default function Ambience({}) {
   return (
     <>
       <img
-        className="fixed z-0 top-0 left-[-25%] opacity-75 animate-breathe pointer-events-none"
-        src="/ambience-left.png"
+        className="fixed z-0 top-[60%] left-[50%] opacity-20 lg:opacity-40 animate-breathe pointer-events-none"
+        src="/bg-blue-layer.svg"
         loading="lazy"
       />
       <img
-        className="fixed z-0 top-[50%] lg:top-0 right-[-25%] md:right-[-50%] opacity-50 animate-breathe pointer-events-none"
-        src="/ambience-right.png"
+        className="fixed z-0 top-[50%] lg:top-[25%] right-[0] md:right-[-40%] opacity-20 lg:opacity-40 animate-breathe pointer-events-none"
+        src="/bg-pink-layer.svg"
         loading="lazy"
       />
     </>
