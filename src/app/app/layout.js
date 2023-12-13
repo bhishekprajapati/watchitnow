6e0ee1acd3a247a961830ad3d9e9ad6d96863564
Abhishek Prajapati <bhishekprajapati@gmail.com>
2023-12-13 16:27:35 +0530

    fix layout on bigger screens

--- src/app/app/layout.js
+++ src/app/app/layout.js
@@ -5,20 +5,22 @@ import Ambience from "@/components/Ambience";
 export default function DashboardLayout({ children }) {
   return (
     <>
-      <Ambience />
-      <div className="lg:w-screen lg:h-[100dvh] lg:overflow-hidden md:p-[1.56rem] lg:p-8 lg:py-0 lg:pr-0 lg:flex">
-        <header className="lg:py-8">
-          <NavBar />
-        </header>
-        <main className="flex-1 lg:overflow-y-auto scrollbar-track-dark-blue scrollbar-thumb-greyish-blue scrollbar-thin">
-          <div className="pt-6 px-5 md:p-0 lg:w-full lg:h-full">
-            <div className="relative z-[1000] mb-4 md:mb-4 lg:sticky lg:top-0 lg:left-0 lg:right-0">
-              <SearchBar />
+      <div className="2xl:max-w-[2000px] 2xl:mx-auto">
+        <Ambience />
+        <div className="lg:w-screen xl:w-full lg:h-[100dvh] lg:overflow-hidden md:p-[1.56rem] lg:p-8 lg:py-0 lg:pr-0 lg:flex">
+          <header className="lg:py-8">
+            <NavBar />
+          </header>
+          <main className="flex-1 lg:overflow-y-auto scrollbar-track-dark-blue scrollbar-thumb-greyish-blue scrollbar-thin">
+            <div className="pt-6 px-5 md:p-0 lg:w-full lg:h-full">
+              <div className="relative z-[1000] mb-4 md:mb-4 lg:sticky lg:top-0 lg:left-0 lg:right-0">
+                <SearchBar />
+              </div>
+              {/* main content area */}
+              <div className="pb-2 lg:px-8 lg:pb-8">{children}</div>
             </div>
-            {/* main content area */}
-            <div className="pb-2 lg:px-8 lg:pb-8">{children}</div>
-          </div>
-        </main>
+          </main>
+        </div>
       </div>
     </>
   );
