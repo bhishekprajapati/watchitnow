070291ea56b84cedee715d6869c2f6fa5932837b
Abhishek Prajapati <bhishekprajapati@gmail.com>
2023-12-18 00:19:55 +0530

    add avatar and logout button in navbar

--- src/app/app/NavBar.js
+++ src/app/app/NavBar.js
@@ -8,18 +8,30 @@ import {
   IconDeviceTv,
   IconBookmark,
 } from "@tabler/icons-react";
+import { Avatar } from "@nextui-org/avatar";
+import { Spinner } from "@nextui-org/spinner";
+import {
+  Dropdown,
+  DropdownTrigger,
+  DropdownMenu,
+  DropdownItem,
+} from "@nextui-org/dropdown";
 import { usePathname } from "next/navigation";
+import { useSession } from "next-auth/react";
+import { IconLogout } from "@tabler/icons-react";
+import { signOut } from "next-auth/react";
 
 export default function NavBar() {
   const pathName = usePathname();
+  const session = useSession();
 
   return (
-    <nav className="px-4 py-[1.12rem] bg-gradient-to-tr md:bg-gradient-to-b from-semi-dark-blue/10 to-semi-dark-blue/50 md:rounded-2xl lg:h-full lg:px-8 lg:py-9 flex lg:flex-col justify-between">
+    <nav className="px-4 py-[1.12rem] bg-gradient-to-tr md:bg-gradient-to-b from-semi-dark-blue/10 to-semi-dark-blue/50 md:rounded-2xl lg:h-full lg:px-8 lg:py-9 flex lg:flex-col lg:items-center justify-between">
       <Link href="/">
         <Image width={24} height={24} src="/logo.svg" alt="site-logo" />
       </Link>
 
-      <ul className="flex gap-x-5 md:gap-x-6 lg:block lg:gap-x-0 lg:[&>:not(:last-child)]:mb-8">
+      <ul className="flex items-center gap-x-5 md:gap-x-6 lg:flex-col lg:gap-y-6">
         <li>
           <Link href="/app/home">
             <IconHome
@@ -59,6 +71,44 @@ export default function NavBar() {
               fill="true"
             />
           </Link>
+        </li>
+
+        <li>
+          {session.status === "loading" && <Spinner />}
+
+          {session.status === "authenticated" && (
+            <Dropdown backdrop="blur">
+              <DropdownTrigger>
+                <Avatar
+                  showFallback
+                  src={session.data.user.image}
+                  name={session.data.user.name}
+                  size="sm"
+                  isBordered
+                  color="success"
+                />
+              </DropdownTrigger>
+              <DropdownMenu
+                variant="faded"
+                className="!right-0"
+                aria-label="Account Actions"
+              >
+                <DropdownItem
+                  key="delete"
+                  className="text-danger"
+                  color="danger"
+                  startContent={<IconLogout size={16} />}
+                  onClick={() => signOut({})}
+                >
+                  Sign out
+                </DropdownItem>
+              </DropdownMenu>
+            </Dropdown>
+          )}
+
+          {session.status === "unauthenticated" && (
+            <Avatar name="Guest" isBordered />
+          )}
         </li>
       </ul>
     </nav>
