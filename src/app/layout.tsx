5ed720f0d63f7c7f65c05cdb88b537e6a83fd462
Abhishek Prajapati <bhishekprajapati@gmail.com>
2024-03-09 09:36:12 +0530

    add analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import "./globals.css";
 import { Providers } from "./providers";
 import { Outfit } from "next/font/google";
 import NextTopLoader from "nextjs-toploader";
+import { Analytics } from "@vercel/analytics/react";
 
 const outfit = Outfit({ style: ["normal"], preload: true, subsets: ["latin"] });
 
@@ -12,6 +13,7 @@ export default function RootLayout({ children }: { children: ReactNode }) {
       <body className={outfit.className}>
         <NextTopLoader />
         <Providers>{children}</Providers>
+        <Analytics />
       </body>
     </html>
   );
